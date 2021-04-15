e21fd3b4cd6c1675706d6a2addac7ac515792c4b
Dominick Hernandez <dominick.hernandez@kohls.com>
2021-04-14 20:38:45 -0500

    Fixed utils

--- main/space_rocks_asteroido/space_rocks/utils.py
+++ main/space_rocks_asteroido/space_rocks/utils.py
@@ -1,7 +1,7 @@
 from pygame.image import load
 
 def load_sprite(name, with_alpha=True): #This loads the images in the following directory as the background.
-    path = f"/Users/tkma4f8/asteroido/main/space_rocks_asteroido/assets/sprites/space.png"
+    path = f"/Users/tkma4f8/asteroido/main/space_rocks_asteroido/assets/sprites/{name}.png"
     loaded_sprite = load(path)
 
     if with_alpha: #Loads the format to fit screen via convert alpha
